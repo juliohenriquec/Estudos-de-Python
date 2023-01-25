27bc0be108bfbf318bc9c517ee62ce61ea401d93
julio <juliohm37@outlook.com>
2023-01-25 15:23:37 -0300

    24/01/2023

--- exercicios/ex069.py
+++ exercicios/ex069.py
@@ -1,1 +1,23 @@
-aasassasasa
+print('PROGRAMA DE CADASTRO DE PESSOAS')
+tot18 = totH = totM20 = 0
+while True:
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]:')).strip().upper()[0]
+    while sexo not in 'MF':
+        sexo = str(input('Sexo [M/F]:')).strip().upper()[0]
+    if idade > 18:
+        tot18 += 1
+    if sexo == 'M':
+        totH += 1
+    if sexo == 'F' and idade < 20:
+        totM20 += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar?[S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'O total de pessoas cadastradas com mais de 18 anos é {tot18}')
+print(f'O total de homens cadastrados é {totH}')
+print(f'O toal de mulheres cadastradas com menos de 20 anos é {totM20}')
+
+
