352ffcf3350c95bdac91b5a2beb9500b356bf538
julio <juliohm37@outlook.com>
2023-01-25 15:03:39 -0300

    24/01/2023

--- exercicios/ex066.py
+++ exercicios/ex066.py
@@ -1,4 +1,4 @@
-n = soma = cont = 0
+soma = cont = 0
 while True:
     n = int(input('Digite um valor[999 para parar]: '))
     if n == 999:
--- exercicios/ex068.py
+++ exercicios/ex068.py
@@ -1,35 +1,28 @@
 from random import randint
 cont = 0
-IA = randint(1, 10)
 print('---Jogo do Par ou Ímpar!---')
 while True:
     num = int(input('Digite um número:'))
-    escolha = str(input('Voce quer par ou ímpar? ')).strip().upper()
+    IA = randint(1, 10)
+    escolha = str(input('Voce quer par ou ímpar?[P/I] ')).strip().upper()[0]
     soma = IA + num
-    print(f'Jogada do Adversário:{IA}')
-    if escolha == 'PAR':
-        computador = 'ÍMPAR'
-    if escolha == 'ÍMPAR':
-        computador = 'PAR'
+    print(f'Você jogou {num} e o computador {IA}. O total foi {soma}.')
+    print('DEU PAR!')
     if soma % 2 == 0:
-        print(f'O resultado é {soma}.Deu par!')
-        if escolha == 'PAR':
+        if escolha == 'P':
             print('Você venceu!')
             cont += 1
         else:
             print('Você perdeu!')
             break
-    if soma % 2 != 0:
-        print(f'O resultado é {soma}.Deu ímpar!')
-        if escolha == 'ÍMPAR':
+    elif soma % 2 != 0:
+        if escolha == 'I':
             print('Você venceu!')
-            cont+= 1
+            cont += 1
         else:
             print('Você perdeu!')
             break
+    print('Vamos jogar novamente...')
 print(f'FIM DE JOGO')
-if cont > 0:
-    print(f'Você venceu por {cont} vezes consecutivas!')
-
-
+print(f'Você venceu por {cont} vezes consecutivas!')
 
