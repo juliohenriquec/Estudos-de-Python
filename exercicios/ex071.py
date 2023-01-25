9582a5ab0491c4fbc60b10f26fe752f641670221
julio <juliohm37@outlook.com>
2023-01-25 16:40:06 -0300

    24/01/2023

---
+++ exercicios/ex071.py
@@ -0,0 +1,29 @@
+print('=' * 30)
+print('{:^30}'.format('BANCO BANCO'))
+print('=' * 30)
+valor = int(input('Qual a quantia a ser sacada?R$'))
+total = valor
+ced = 100
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} cédulas de R${ced}')
+        if ced == 100:
+            ced = 50
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        elif ced == 5:
+            ced = 2
+        elif ced == 2:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
